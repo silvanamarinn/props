b9bc3699d50ff2b48035a256ddc9fa917fe66e0a
Silvana Marin <silvana.marinc@gmailcom>
2022-02-02 15:48:49 -0300

    propsdos

---
+++ propssilvana/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <section>
+      <label for="txt_task">Tarea</label>
+      <input id="txt_task" type="text" v-model="task" placeholder="ingresar nueva tarea"/>
+      
+      <button @click="addTask">Añadir</button>
+      
+    </section>
+    <section>
+      <Lista :tasks="tasks" :deleteTaks="deleteTaks" />
+    </section>
+  </div>
+</template>
+
+<script>
+import Lista from '@/components/Lista.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Lista
+  },
+    data() {
+    return {
+      task: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      const { task } = this;
+      this.tasks.push(task);
+    },
+    deleteTaks(indexTask) {
+      this.tasks.splice(indexTask, 1);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Courier New', Courier, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 30px;
+}
+
+label {
+  font-weight: 700;
+  margin: 0.3rem
+}
+
+button {
+  padding: 0.1rem 0.75rem;
+  margin: 0rem 0.3rem;
+  border-radius: 8px;
+  border: 1px solid rgb(204, 202, 202);
+  background-color: white;
+}
+
+ul {
+  font-weight: 700;
+}
+
+section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ propssilvana/src/components/Lista.vue
@@ -0,0 +1,21 @@
+<template>
+<ul>
+<li v-for="(task, i) in tasks" :key="i">
+{{ task }}
+<button @click="deleteTaks(i)">Eliminar</button>
+</li>
+</ul>
+</template>
+
+<script>
+export default {
+name: "Lista",
+props: {
+tasks: [],
+deleteTaks: { type: Function },},
+};
+</script>
+	
+
+<style>
+</style>
